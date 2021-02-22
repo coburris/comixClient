7ed04caea8b1ca764c287eabc279dd018c068e20
Steve Emerson <sbemerson@outlook.com>
2021-02-22 05:43:03 -0500

    Edit modal complete

--- src/shelf/Comic.jsx
+++ src/shelf/Comic.jsx
@@ -13,7 +13,15 @@ const Comic = (props) => {
   const [editOpen, setEditOpen] = useState(false);
   const [comicStatus, setComicStatus] = useState(props.comic.status);
   const [comicName, setComicName] = useState(props.comic.issue_name);
-
+  const [comicVolume, setComicVolume] = useState(props.comic.volume_name);
+  const [comicIssueNumber, setComicIssueNumber] = useState(props.comic.issue_number);
+  const [comicPublisher, setComicPublisher] = useState(props.comic.publisher);
+  const [comicCoverDate, setComicCoverDate] = useState(props.comic.cover_date);
+  const [comicCharacters, setComicCharacters] = useState(props.comic.characters);
+  const [comicTeams, setComicTeams] = useState(props.comic.teams);
+  const [comicStoryArcs, setComicStoryArcs] = useState(props.comic.story_arcs);
+  const [comicDescription, setComicDescription] = useState(props.comic.description);
+  
   
 
   //Functions
@@ -54,14 +62,23 @@ const Comic = (props) => {
     
     let server_url = `http://localhost:3000/shelf/${props.comic.id}`
 
-    console.log(server_url);
+    console.log(comicCoverDate);
     
     let newComicData = 
       {
         "issue_name": comicName,
-        "status" : comicStatus
-        
+        "status" : comicStatus,
+        "volume_name" : comicVolume,
+        "issue_number" : comicIssueNumber,
+        "publisher": comicPublisher,
+        "cover_date": comicCoverDate,
+        "characters": comicCharacters,
+        "teams": comicTeams,
+        "story_arcs": comicStoryArcs,
+        "description": comicDescription
       }
+
+    console.log(newComicData);
 
     fetch(server_url, {
       method: 'PUT',
@@ -80,13 +97,25 @@ const Comic = (props) => {
     })
     .catch(err => console.log(`Failed comic edit: ${err}`));
 
-    toggleEditForm()
+    toggleEditForm();
     
   }
 
   function displayEditModal() {
-    toggle()
-    toggleEditForm()
+    toggle();
+    toggleEditForm();
+  }
+
+  function dateFormat(d){
+    let year = d.getFullYear();
+    let month = d.getMonth() + 1;
+    let day = d.getDate();
+
+    month = (month < 10 ) ? `0${month}` : month;
+    day = (day < 10) ? `0${day}` : day;
+
+
+    return `${year}-${month}-${day}`;
   }
 
   
@@ -239,45 +268,91 @@ const Comic = (props) => {
                 <option value="2">Read</option>
               </Input>
             </FormGroup>
+            <FormGroup>
+              <Label for="comic-volume">Volume</Label>
+              <Input 
+              type="text" 
+              name="comic-volume" 
+              id="comic-volume" 
+              defaultValue={props.comic.volume_name}
+              onChange={(e) => setComicVolume(e.target.value)} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="comic-issue-number">Issue Number</Label>
+              <Input 
+              type="text" 
+              name="comic-issue-number" 
+              id="comic-issue-number" 
+              defaultValue={props.comic.issue_number}
+              onChange={(e) => setComicIssueNumber(e.target.value)} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="comic-publisher">Publisher</Label>
+              <Input 
+              type="text" 
+              name="comic-publisher" 
+              id="comic-publisher" 
+              defaultValue={props.comic.publisher}
+              onChange={(e) => setComicPublisher(e.target.value)} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="comic-cover-date">Cover Date</Label>
+              <Input 
+              type="date" 
+              name="comic-cover-date" 
+              id="comic-cover-date" 
+              defaultValue={props.comic.cover_date}
+              min="1900-01-01"
+              max={dateFormat(new Date())}
+              onChange={(e) => setComicCoverDate(e.target.value)} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="comic-characters">Characters</Label>
+              <Input 
+              type="textarea" 
+              name="comic-characters" 
+              id="comic-characters" 
+              defaultValue={(props.comic.characters) ? props.comic.characters.join("\n") : ""}
+              rows={(props.comic.characters) ? props.comic.characters.length + 2 : 2}
+             
+              onChange={(e) => setComicCharacters(e.target.value.split("\n"))} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="comic-teams">Teams</Label>
+              <Input 
+              type="textarea" 
+              name="comic-teams" 
+              id="comic-teams" 
+              defaultValue={(props.comic.teams) ? props.comic.teams.join("\n") : ""}
+              rows={(props.comic.teams) ? props.comic.teams.length + 2 : 2}
+              onChange={(e) => setComicTeams(e.target.value.split("\n"))} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="comic-story-arcs">Story Arcs</Label>
+              <Input 
+              type="textarea" 
+              name="comic-story-arcs" 
+              id="comic-story-arcs" 
+              defaultValue={(props.comic.story_arcs) ? props.comic.story_arcs.join("\n") : ""}
+              rows={(props.comic.story_arcs) ? props.comic.teams.story_arcs + 2 : 2}
+              onChange={(e) => setComicStoryArcs(e.target.value.split("\n"))} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="comic-description">Description</Label>
+              <Input 
+              type="textarea" 
+              name="comic-description" 
+              id="comic-description" 
+              defaultValue = {props.comic.description}
+              rows={10}
+              onChange={(e) => setComicDescription(e.target.value)} />
+            </FormGroup>
           </Form>
-        {/* <p>Name: <strong>{props.comic.issue_name}</strong></p>
-        <span style={{fontSize:"1rem"}}> 
-              {props.comic.volume_name} #{props.comic.issue_number}
-            </span>
-          <div className='comic-publisher'>
-            <p> <strong>Publisher: </strong>
-              {(props.comic.publisher && props.comic.publisher.length > 0)
-              ? props.comic.publisher
-              : 'No publisher listed. EDIT to add.'} 
-            </p>
-          </div>
-          <div className='comic-date'>
-            <p> <strong>Cover Date: </strong>
-              {(props.comic.cover_date && props.comic.cover_date.length > 0)
-              ? props.comic.cover_date
-              : 'No date listed. EDIT to add.'} 
-            </p>
-          </div>
-          <div className='comic-characters'>
-            <p> <strong>Characters: </strong>
-              {(props.comic.characters && props.comic.characters.length > 0)
-              ? props.comic.characters.join(', ')
-              : 'No characters listed. EDIT to add.'} 
-            </p>
-          </div>
-          <div className='comic-teams'>
-            <p> <strong>Teams: </strong>
-              {(props.comic.teams && props.comic.teams.length > 0)
-                ? props.comic.teams.join(', ')
-                : 'No teams listed. EDIT to add.'} 
-            </p>
-          </div>
-          <div className='comic-stories'>
-            <p> <strong>Story Arcs: </strong>
-              {(props.comic.story_arcs && props.comic.story_arcs.length > 0)
-                ? props.comic.story_arcs.join(', ')
-                : 'No story arcs listed. EDIT to add.'} 
-            </p>
+          
+        {/* 
+              
+        
+        
           </div>
           <div className='comic-desc' style={{borderTop:"solid 1px"}}>
             {ReactHtmlParser(props.comic.description)}
