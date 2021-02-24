c94f15cff0cd8c8dd653dad8274cdc715f812974
Daniel Buergler <daniel.jbuergler@gmail.com>
2021-02-23 21:13:30 -0500

    random changes

--- src/auth/Auth.jsx
+++ src/auth/Auth.jsx
@@ -31,7 +31,7 @@ const Auth = (props) => {
     <Router>
         <Container className="auth-container" style={authStyle}>
             <Row>
-                <Col md="6">{showToggle === true ? <Signup updateToken={props.updateToken} setUser={props.setUser}/> : <Login updateToken={props.updateToken}  setUser={props.setUser}/>}</Col>
+                <Col md="6">{showToggle === true ? <Signup updateToken={props.updateToken} setUser={props.setUser}/> : <Login updateToken={props.updateToken} setUser={props.setUser}/>}</Col>
             </Row>
                 <Button onClick={handleToggle} style={toggleButtonStyle}>Toggle Signup and Login</Button>
         </Container>
