cc96eba04159d689753ce590b20d7e083c7f1df9
Collin Burris <collinburris94@gmail.com>
2021-03-02 21:07:57 -0500

    added styling to search page

--- src/search/SearchComic.jsx
+++ src/search/SearchComic.jsx
@@ -5,6 +5,7 @@ Card, CardImg, CardText, CardBody, CardDeck, CardTitle, CardSubtitle,
 Button, Modal, ModalHeader, ModalBody, ModalFooter, Tooltip, 
 Collapse, Form, Input, FormGroup, Label, CardGroup} from 'reactstrap';
 import SearchPage from './SearchPage'
+import './SearchComic.css'
 
 
 const SearchComic = (props) => {
@@ -83,7 +84,6 @@ const cardStyle =
     minWidth: "100px",
     postion:"absolute", 
     //   bottom:"0px"
-
     }
 
 const cardImageStyle = 
