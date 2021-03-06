2fc95ecf531a3bc2bcafece463f9729d85188135
Steve Emerson <sbemerson@outlook.com>
2021-03-06 03:50:52 -0500

    cleanup

--- src/shelf/RandomComic.jsx
+++ src/shelf/RandomComic.jsx
@@ -32,8 +32,9 @@ function RandomComic(props) {
     let api_key = "10b174a86660d99247de4c3b2117f611aecc1625";
     //let comic_id = `4000-${Math.floor(Math.random()*100000)}`
 
-    let index = Math.floor((Math.random())*safeComics.length);
-    let comic_id = `4000-${safeComics[index]}`;
+    // SAFE SEARCH
+    // let index = ;
+    let comic_id = `4000-${safeComics[Math.floor((Math.random())*safeComics.length)]}`;
 
     let heroku_cors = "efa-cors-anywhere.herokuapp.com/";
     let url = `https://${heroku_cors}comicvine.gamespot.com/api/issue/${comic_id}/?api_key=${api_key}&format=json`
